887405d48d7ba248f6c8b6c53340ec7fa120ebf9
grandhimanikanta <manikanta.mani.g@gmail.com>
2020-10-28 18:20:04 +0530

    Add files via upload

---
+++ assesment.py
@@ -0,0 +1,24 @@
+d={}
+f=open("data.txt","r")
+for s in f:
+    # print(s)
+    l=s.split()
+    # print(l)
+    for i in l:
+        if i.startswith("#"):
+            if i in d:
+                d[i]=d[i]+1
+                # print(d)
+            else:
+                d[i]=1
+                # print(d)
+    sorted_d = sorted(d.items(), key=lambda x: x[1])
+f.close()
+print(d)
+count=0
+for key,values in d.items():
+    if count !=10:
+        print(key,values)
+        count=count+1
+    else:
+        break
